fd9246906a73b2f8abd3c7ff27b47eaca8a118b5
kimth815 <suksudong123@gmail.com>
2026-02-12 16:17:09 +0900

    Convert Python login function to JavaScript

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-# 테스트용 취약 코드 예시 (Python)
-def login(password):
-    if password == "123456":  # CodeQL이 이 하드코딩된 비밀번호를 잡아내야 합니다.
-        print("Logged in!")
+// test.js
+function login(password) {
+  if (password === "123456") { // 하드코딩된 비밀번호 취약점 유발
+    console.log("Logged in!");
+  }
+}
